e38a039d5412092252145b5bc7c93af1a5cdd4e0
Jude <jhertting@gmail.com>
2025-04-02 11:13:57 -0500

    Added CheckWin(), CheckTie() functions. Updated IsGameOver() to check for win/tie conditions

--- CPP-TicTacToe/TicTacToe.cpp
+++ CPP-TicTacToe/TicTacToe.cpp
@@ -1,10 +1,58 @@
 #include "TicTacToe.h"
 #include <iostream>
 
-TicTacToe::TicTacToe()
+// initialize the board display
+TicTacToe::TicTacToe() : m_playerTurn(1)
 {
+	for (int i = 1; i < 10; i++)
+	{
+		m_board[i] = 1 + i;
+	}
 }
 
-TicTacToe::TicTacToe()
+bool TicTacToe::IsGameOver() const
 {
+	return CheckWin() || CheckTie();
 }
+
+bool TicTacToe::CheckWin() const
+{
+	// define all possible win conditions 
+	int winConditions[8][3] =
+	{
+		// row
+		{1,2,3}, {4, 5, 6}, {7, 8, 9},
+
+		// column
+		{1, 4, 7}, {2, 5, 8}, {3, 6, 9},
+
+		// diagonal
+		{1,5,9}, {3,5,7}
+	};
+
+	// checks each possible pattern for a win condition
+	for (const auto& condition : winConditions) 
+	{
+		char initialMark = m_board[condition[0]];
+
+		if (initialMark != ' ' && initialMark == m_board[condition[1]] && initialMark == m_board[condition[2]])
+		{
+			return true; 
+		}
+	}
+	return false;
+}
+
+bool TicTacToe::CheckTie() const
+{
+	// if no empty spaces remain = tie
+	for (int i = 1; i < 10; ++i) {
+		if (m_board[i] != 'X' && m_board[i] != 'O') 
+		{
+			return false;  
+		}
+	}
+	return true;
+}
+
+
